2a12696dd6d0f8593ec3ba4d7693f71a42b4e6c1
ETomi24 <tamas.ernyei@gmail.com>
2022-03-12 18:20:05 +0100

    Repository Annotation added

--- src/main/java/hu/bme/aut/shed/repository/UserRepository.java
+++ src/main/java/hu/bme/aut/shed/repository/UserRepository.java
@@ -2,9 +2,11 @@ package hu.bme.aut.shed.repository;
 
 import hu.bme.aut.shed.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findByID(String s);
