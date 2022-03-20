91e15c63efee0fe8b93fb7d3be5a771e9d2e7b29
Tomi <pintertamas99@gmail.com>
2022-03-20 21:54:06 +0100

    SecurityConfig.java deleted and merged the relevant lines into WebSecurityConfig.java

--- src/main/java/hu/bme/aut/shed/config/WebSecurityConfig.java
+++ src/main/java/hu/bme/aut/shed/config/WebSecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -51,6 +50,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
+                .requiresChannel()
+                .anyRequest().requiresSecure()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/login", "/register")
                 .permitAll()
