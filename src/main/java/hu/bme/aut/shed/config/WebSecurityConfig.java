fadd7727b34daf831b2fa588014120a7317937dc
Tomi <pintertamas99@gmail.com>
2022-03-29 18:59:37 +0200

    cors works

--- src/main/java/hu/bme/aut/shed/config/WebSecurityConfig.java
+++ src/main/java/hu/bme/aut/shed/config/WebSecurityConfig.java
@@ -75,7 +75,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("*").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
