7058cc37d2fda88e1b3c3058aaaab57d8174a043
ETomi24 <tamas.ernyei@gmail.com>
2022-04-02 22:59:47 +0200

    Websocket in proggress HUPS

--- src/main/java/hu/bme/aut/shed/config/WebSecurityConfig.java
+++ src/main/java/hu/bme/aut/shed/config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors().and()//.csrf().disable()
+        httpSecurity.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login", "/register", "/check-token-validity", "/game/create" , "game/start")
                 .permitAll()
