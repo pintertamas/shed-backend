9c303a23a5f8680ed432f347e2004dffbc961266
ETomi24 <tamas.ernyei@gmail.com>
2022-04-02 22:56:46 +0200

    Websocket in proggress OMGMGMGMMG

---
+++ src/main/java/hu/bme/aut/shed/config/WebSocketSecurityConfig.java
@@ -0,0 +1,24 @@
+package hu.bme.aut.shed.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .simpTypeMatchers(SimpMessageType.CONNECT, SimpMessageType.SUBSCRIBE, SimpMessageType.UNSUBSCRIBE, SimpMessageType.DISCONNECT).permitAll()
+                .anyMessage().permitAll()
+                .simpDestMatchers("/**").permitAll();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
