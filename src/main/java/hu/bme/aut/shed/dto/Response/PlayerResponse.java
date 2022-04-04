685c851f863f7888934a22617edd188b1fb380ed
Pinter Tamas <pintertamas99@gmail.com>
2022-04-04 22:31:06 +0200

    players cannot connect multiple times

--- src/main/java/hu/bme/aut/shed/dto/Response/PlayerResponse.java
+++ src/main/java/hu/bme/aut/shed/dto/Response/PlayerResponse.java
@@ -1,7 +1,11 @@
 package hu.bme.aut.shed.dto.Response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @AllArgsConstructor
 public class PlayerResponse {
     String username;
