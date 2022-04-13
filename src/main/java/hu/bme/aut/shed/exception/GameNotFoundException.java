fea19b705a7556812031d4b8ecca91446357ab36
ETomi24 <tamas.ernyei@gmail.com>
2022-04-14 00:44:35 +0200

    GameNotFoundException test change

--- src/main/java/hu/bme/aut/shed/exception/GameNotFoundException.java
+++ src/main/java/hu/bme/aut/shed/exception/GameNotFoundException.java
@@ -2,6 +2,6 @@ package hu.bme.aut.shed.exception;
 
 public class GameNotFoundException extends Exception {
     public GameNotFoundException() {
-        super("Could not find game with the provided ID!");
+        super("game-not-found");
     }
 }
