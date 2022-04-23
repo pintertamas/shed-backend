09d523f733cc3eef53b6692df674d899b7be03d7
ETomi24 <tamas.ernyei@gmail.com>
2022-04-23 15:55:55 +0200

    PlayerCardState TableCardState now stored correctly

---
+++ src/main/java/hu/bme/aut/shed/model/Converter/PlayerCardStateConverter.java
@@ -0,0 +1,20 @@
+package hu.bme.aut.shed.model.Converter;
+
+import hu.bme.aut.shed.model.PlayerCardState;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class PlayerCardStateConverter implements AttributeConverter<PlayerCardState,String> {
+
+    @Override
+    public String convertToDatabaseColumn(PlayerCardState playerCardState) {
+        return playerCardState.getName();
+    }
+
+    @Override
+    public PlayerCardState convertToEntityAttribute(String dbData) {
+        return PlayerCardState.fromName(dbData);
+    }
+}
--- src/main/java/hu/bme/aut/shed/model/TableCardState.java
+++ src/main/java/hu/bme/aut/shed/model/TableCardState.java
@@ -1,5 +1,30 @@
 package hu.bme.aut.shed.model;
 
 public enum TableCardState {
-    PICK , THROW
+    PICK ("Pick"),
+    THROW ("Throw");
+
+    private final String name;
+
+    TableCardState(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static TableCardState fromName(String name) throws IllegalArgumentException {
+        switch (name) {
+            case "Pick":
+                return TableCardState.PICK;
+
+            case "Throw":
+                return TableCardState.THROW;
+
+            default:
+                throw new IllegalArgumentException("TableCardState [" + name
+                        + "] not supported.");
+        }
+    }
 }
