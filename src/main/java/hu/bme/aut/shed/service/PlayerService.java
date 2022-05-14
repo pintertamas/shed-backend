6c58464b64137c6e0e190260833bbbad6e5fe46f
ETomi24 <tamas.ernyei@gmail.com>
2022-05-14 15:36:06 +0200

    playerFinishedPosition set by a method

--- src/main/java/hu/bme/aut/shed/service/PlayerService.java
+++ src/main/java/hu/bme/aut/shed/service/PlayerService.java
@@ -182,14 +182,19 @@ public class PlayerService {
     }
 
     @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public void setPlayerFinishedPosition(Player player , Game game){
+        player.setStatus(GameStatus.FINISHED);
+        List<Player> finishedPlayers = this.getAllPlayersByStateAndGame(GameStatus.FINISHED , game);
+        player.setFinishedPosition(finishedPlayers.size() + 1);
+        LoggerFactory.getLogger(this.getClass()).info("Player is Finished" + String.valueOf(player.getUsername()) + "Position :" + String.valueOf(player.getFinishedPosition()) );
+        playerRepository.save(player);
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
     public void checkEndCondition(Player player , Game game){
         int pickCardsSize = tableCardService.getAllByTableCardStateAndGame(TableCardState.PICK,game).size();
         if(player.getCards().size() == 0 && pickCardsSize == 0){
-            player.setStatus(GameStatus.FINISHED);
-            List<Player> finishedPlayers = this.getAllPlayersByStateAndGame(GameStatus.FINISHED , game);
-            player.setFinishedPosition(finishedPlayers.size() + 1);
-            LoggerFactory.getLogger(this.getClass()).info("Player is Finished" + String.valueOf(player.getUsername()) + "Position :" + String.valueOf(player.getFinishedPosition()) );
-            playerRepository.save(player);
+            this.setPlayerFinishedPosition(player , game);
         }
     }
 
